e59d88a516e6b0439fa4be93d600af380d4c11a1
Bruno Lagoa <miroldols@gmail.com>
2015-06-22 18:30:29 -0300

    - Criando atributos em pessoa.php  - O COMEÇO - Atributos

--- pessoa.php
+++ pessoa.php
@@ -3,7 +3,17 @@
 class Pessoa
 {
 
+    public $nome;
+    public $idade;
+
 }
 
 $pessoa1 = new pessoa();
+$pessoa1->nome = "Bruno";
+$pessoa1->idade = 30;
+
 $pessoa2 = new pessoa();
+$pessoa2->nome = "Cesar";
+$pessoa2->idade = 20;
+
+echo $pessoa1->nome;
