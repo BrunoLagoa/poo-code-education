ef1c74146fb06275159512ad52ee5d0545ff0ec7
Bruno Lagoa <miroldols@gmail.com>
2015-06-22 18:37:38 -0300

    - Criando métodos em pessoa.php  - O COMEÇO - Métodos

--- pessoa.php
+++ pessoa.php
@@ -6,6 +6,11 @@ class Pessoa
     public $nome;
     public $idade;
 
+    public function correr()
+    {
+        echo $this->nome . " correu!";
+    }
+
 }
 
 $pessoa1 = new pessoa();
@@ -16,4 +21,4 @@ $pessoa2 = new pessoa();
 $pessoa2->nome = "Cesar";
 $pessoa2->idade = 20;
 
-echo $pessoa1->nome;
+echo $pessoa1->correr();
