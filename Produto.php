2a433505e014515b99db38c5a87424809911c721
Bruno Lagoa <miroldols@gmail.com>
2015-06-23 00:22:46 -0300

    - Criando os Getters e Setters em Produto.php  - MODIFICADORES DE ACESSO - Getters e Setters

--- Produto.php
+++ Produto.php
@@ -3,9 +3,73 @@
 class Produto
 {
     private $nome;
-    public $descricao;
-    public $valor;
-    public $estoque;
+    private $descricao;
+    private $valor;
+    private $estoque;
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    /**
+     * @param mixed $descricao
+     */
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    /**
+     * @param mixed $valor
+     */
+    public function setValor($valor)
+    {
+        $this->valor = $valor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEstoque()
+    {
+        return $this->estoque;
+    }
+
+    /**
+     * @param mixed $estoque
+     */
+    public function setEstoque($estoque)
+    {
+        $this->estoque = $estoque;
+    }
 
     /*
     public function __construct($nome, $descricao, $valor, $estoque)
@@ -17,6 +81,8 @@ class Produto
 
     }
     */
+
+
 
     private function baixaEstoque()
     {
