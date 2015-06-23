dae6169378f04e29989cc50b8973b97c865774a8
Bruno Lagoa <miroldols@gmail.com>
2015-06-22 23:07:28 -0300

    - Verificando se uma herança é do mesmo tipo  - TIPOS, HERANÇA E INTERFACES - Tipos vs herança

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 
-require_once "Pessoa.php";
-
-$pessoa1 = new Pessoa("Bruno",30);
-
-$pessoa2 = new Pessoa("Cesar",20);
-
-echo $pessoa1->correr(50);
+//require_once "Pessoa.php";
+//$pessoa1 = new Pessoa("Bruno",30);
+//$pessoa2 = new Pessoa("Cesar",20);
+//
+//echo $pessoa1->correr(50);
 
 require_once "Produto.php";
+require_once "Tenis.php";
 
-$produto1 = new Tenis();
+$tenis = new Tenis();
+
+if($tenis instanceof Produto){
+    echo "E um tipo tenis";
+}else{
+    echo "Nao e um tipo tenis";
+}
