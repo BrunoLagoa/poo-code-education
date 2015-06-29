94a8eeda39c5572b9481f5f103f7b89a782b4701
Bruno Lagoa <miroldols@gmail.com>
2015-06-29 19:00:10 -0300

    ajustando arquivos deletados

--- cadastro-de-clientes/PessoaInterFace.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface PessoaInterFace
-{
-    public function setEstrela($valor);
-    public function getEstrela();
-    public function setEnderecoCobranca($valor);
-    public function getEnderecoCobranca();
-}
