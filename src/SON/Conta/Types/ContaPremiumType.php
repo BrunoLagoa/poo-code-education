bf0f2c3e2d7c247585e2deafc34f016280cd54ac
Bruno Lagoa <miroldols@gmail.com>
2015-06-29 19:09:49 -0300

    - Criando autoload em ContaPremiumType.php

--- src/SON/Conta/Types/ContaPremiumType.php
+++ src/SON/Conta/Types/ContaPremiumType.php
@@ -1,8 +1,10 @@
 <?php
 
-
+define('CLASS_DIR', 'src/');
+set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR);
+spl_autoload_register();
 
 
 $x = new SON\Conta\Types\ContaType();
-$x->despositar(10);
+$x->depositar(10);
 echo $x->getSaldo();
